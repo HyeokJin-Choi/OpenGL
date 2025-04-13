08761000df230142652bbe418ea4ed24892b01ef
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-04-13 12:52:50 +0900

    Update testHardCode.cpp

--- OpenGL/test/testHardCode.cpp
+++ OpenGL/test/testHardCode.cpp
@@ -1,1 +1,37 @@
+#include <GL/glut.h>
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
 
+#define GL_PI 3.1415f
+
+void glPointSize(GLfloat s);
+
+float getRandomFloat() { 
+    return ;
+}
+
+void RenderScene() {
+	
+}
+
+void SetupRC(void) {
+	std::cout << "SetupRC" << std::endl;
+}
+
+void changeSize(GLsizei w, GLsizei h) {
+	
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	// glutInitDisplayMode( | GLUT_RGB);
+	// glutInitWindowSize(, );
+	// glutInitWindowPosition(, );
+	// glutCreateWindow();
+
+	SetupRC();
+	//glutDisplayFunc();
+	//glutReshapeFunc();
+	glutMainLoop();
+}
