79ad72cf3954d25e8940ecf0cb67e890c70bee2a
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-04-23 23:29:14 +0900

    Update glTest0417.cpp

--- OpenGL/test/variousAttempts/glTest0417.cpp
+++ OpenGL/test/variousAttempts/glTest0417.cpp
@@ -1,85 +1,62 @@
 #include <GL/glut.h>
-#include <stdio.h>
 #include <iostream>
-#include <math.h>
-
-#define GL_PI 3.1415f
-
-void glPointSize(GLfloat s);
-
-void glLineWidth(GLfloat ls);
-
-float getRandomFloat() {
-	return (float)rand() / (float)RAND_MAX;
-}
-
-void RenderScene() {
-	GLfloat x, y, z, angle;
-	GLfloat sizes[2], step;
-	GLfloat lineSize;
-	glGetFloatv(GL_LINE_WIDTH_RANGE, sizes);
-	glGetFloatv(GL_LINE_WIDTH_GRANULARITY, &step);
-
+#define GL_PI 3.141592
+using namespace std;
+void glPointSize(GLfloat size);
+void glLineSize(GLfloat size);
+void RenderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
-
 	glRotatef(45, 1.0f, 0.0f, 0.0f);
-	glRotatef(45, 0.0f, 1.0f, 0.0f); 
+	glRotatef(45, 0.0f, 1.0f, 0.0f);
+	
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f,0.0f,0.0f);
+	glVertex3f(0.0f,75.0f,0.0f);
+	glVertex3f(0.0f, 75.0f, 0.0f);
+	glVertex3f(5.0f, 70.0f, 0.0f);
+	glVertex3f(0.0f, 75.0f, 0.0f);
+	glVertex3f(-5.0f, 70.0f, 0.0f);
 
 	glColor3f(1.0f, 0.0f, 0.0f);
-	lineSize = sizes[0];
-
-	glBegin(GL_LINES);
-	glVertex3f(0.0f,0.0f,0.0f);
-	glVertex3f(100.0f,0.0f,0.0f);
-	glVertex3f(100.0f, 0.0f, 0.0f);
-	glVertex3f(85.0f, 15.0f, 15.0f);
-	glVertex3f(100.0f, 0.0f, 0.0f);
-	glVertex3f(95.0f, 5.0f, -5.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(75.0f, 0.0f, 0.0f);
+	glVertex3f(75.0f, 0.0f, 0.0f);
+	glVertex3f(70.0f, 0.0f, 5.0f);
+	glVertex3f(75.0f, 0.0f, 0.0f);
+	glVertex3f(70.0f, 0.0f, -5.0f);
 
 	glColor3f(0.0f, 0.0f, 1.0f);
 	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 100.0f);
-	glVertex3f(0.0f, 0.0f, 100.0f);
-	glVertex3f(-5.0f, 0.0f, 95.0f);
-	glVertex3f(0.0f, 0.0f, 100.0f);
-	glVertex3f(0.0f, 5.0f, 100.0f);
+	glVertex3f(0.0f, 0.0f, 75.0f);
+	glVertex3f(0.0f, 0.0f, 75.0f);
+	glVertex3f(5.0f, 0.0f, 70.0f);
+	glVertex3f(0.0f, 0.0f, 75.0f);
+	glVertex3f(-5.0f, 0.0f, 70.0f);
 
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 100.0f, 0.0f);
-	glVertex3f(0.0f, 100.0f, 0.0f);
-	glVertex3f(-5.0f, 95.0f, 0.0f);
-	glVertex3f(0.0f, 100.0f, 0.0f);
-	glVertex3f(0.0f, 100.0f, 5.0f);
 	glEnd();
 
 	glPopMatrix();
 	glFlush();
 }
-
 void SetupRC(void) {
-	std::cout << "SetupRC" << std::endl;
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
-
-void changeSize(GLsizei w, GLsizei h) {
-	GLfloat t = 100;
-	GLfloat wSizeRatio;
-	glViewport(0, 0, w, h); // 도화지는 카메라의 투영과 상관없으니, glMatrixMode()함수 안에 들어가지 x
-
+void ChangeSize(GLsizei w, GLsizei h) {
+	GLfloat aspectRatio;
+	GLfloat t = 100.0f;
+	if (h == 0) h = 1;
+	glViewport(0, 0, w, h);
+	aspectRatio = (GLfloat)w / (GLfloat)h;
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
-	wSizeRatio = (GLfloat)w / (GLfloat)h;
-	if (w <= h) { // 세로로 창이 늘어남 -> 투영을 세로로 확장
-		glOrtho(-t, t, -t / wSizeRatio, t / wSizeRatio, t, -t);
+	if (w <= h) {
+		glOrtho(-t, t, -t / aspectRatio, t * aspectRatio, t, -t);
 	}
 	else {
-		glOrtho(-t * wSizeRatio, t * wSizeRatio, -t, t, t, -t);
+		glOrtho(-t * aspectRatio, t * aspectRatio, -t, t, t, -t);
 	}
-
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -88,11 +65,11 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Checkjin");
+	glutInitWindowPosition(400, 400);
+	glutCreateWindow("Checkjin's RenderScene");
 
-	SetupRC();
 	glutDisplayFunc(RenderScene);
-	glutReshapeFunc(changeSize);
+	glutReshapeFunc(ChangeSize);
+	SetupRC();
 	glutMainLoop();
 }
